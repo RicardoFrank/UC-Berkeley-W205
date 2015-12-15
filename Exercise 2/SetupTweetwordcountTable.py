a34321bff1942fc70c01a537dbec3a27a441292f
RicardoFrank <RicardoFrankBarrera@gmail.com>
2015-12-15 02:00:21 -0800

    Exercise 2 submission

--- Exercise 2/SetupTweetwordcountTable.py
+++ Exercise 2/SetupTweetwordcountTable.py
@@ -5,7 +5,7 @@
 
 import psycopg2
 
-conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
+conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 
 #Create a Table
 #The first step is to create a cursor. 
